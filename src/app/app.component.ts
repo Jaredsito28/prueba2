9cb6906778653bde235789a427260647a933568b
Jaredsito28 <carlosjaredlescano28@gmail.com>
2024-07-11 22:56:22 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CalorieCalculatorComponent} from './components/calorie-calculator/calorie-calculator.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalorieCalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
